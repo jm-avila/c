30ca419ec4562fd2bd500d35b7c923735aa56937
jose-avila-007 <jose.avila@rookiewise.com>
2023-10-09 13:06:10 +0200

    random stuff

---
+++ random/integerSize.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("size of a short is %lu\n", sizeof(short));
+	printf("size of a int is %lu\n", sizeof(int));
+	printf("size of a long is %lu\n", sizeof(long));
+}
